a082c9b3488b138ca4c537750e07f7394f20c7d7
Leigh Shayler <leigh.shayler@ignia.com.au>
2018-10-24 16:50:58 +0800

    Cleaned folders, added anonymous test proxy

---
+++ src/Edge/ioBee.Edge.AnonymousProxy/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ioBee.Edge.AnonymousProxy
+{
+    public class Startup
+    {
+        private string _deviceConnectionString;
+        private static TransportType _transportType = TransportType.Amqp;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            //"IOTHUB_DEVICE_CONN_STRING": "HostName=SaveTheBees-Iot-Hub.azure-devices.net;DeviceId=DeviceBeeHive1;SharedAccessKey=ZFcJsSjuoqrzcKwiSFlAy99Cf2zc5t0KnfL3BlDSXCI="
+
+            _deviceConnectionString = configuration["IOTHUB_DEVICE_CONN_STRING"];
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(_deviceConnectionString, _transportType);
+            services.AddSingleton<DeviceClient>(deviceClient);
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.SuppressModelStateInvalidFilter = true;
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
